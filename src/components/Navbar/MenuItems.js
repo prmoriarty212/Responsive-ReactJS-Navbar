1f897920237f638dc5f07256981bfe8373a4c877
Ruslan <81567391+prmoriarty212@users.noreply.github.com>
2022-08-23 17:32:52 +0300

    Added all projects files

---
+++ src/components/Navbar/MenuItems.js
@@ -0,0 +1,27 @@
+export const MenuItems = [
+    {
+        title: 'Home',
+        link: '#',
+        cName: 'nav-links',
+    },
+    {
+        title: 'About Us',
+        link: '#',
+        cName: 'nav-links',
+    },
+    {
+        title: 'Services',
+        link: '#',
+        cName: 'nav-links',
+    },
+    {
+        title: 'Shop Now',
+        link: '#',
+        cName: 'nav-links',
+    },
+    {
+        title: 'Sign Up',
+        link: '#',
+        cName: 'nav-links-mobile',
+    }
+]
